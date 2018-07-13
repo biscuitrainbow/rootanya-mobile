5fdff20de65d4de7254407274d2bdc13e8944259
Natthapon Sricort <natthaponsricort@gmail.com>
2018-07-13 12:42:46 +0700

    Change api

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
 //  static final String url = 'http://10.0.2.2:8000';
-//  static final String url = 'http://178.128.111.90';
-  static final String url = 'http://192.168.2.105';
+  static final String url = 'http://178.128.111.90';
+//  static final String url = 'http://192.168.2.105';
 }
