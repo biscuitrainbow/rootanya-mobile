87625f9d6d6c4756c41dd0dc705056eed929ca0b
Natthapon Sricort <Natthaponsricort@gmail.com>
2018-08-03 00:25:17 +0700

    Remove trailing from request

--- lib/data/network/history_repository.dart
+++ lib/data/network/history_repository.dart
@@ -1,32 +1,36 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:medical_app/config.dart';
 import 'package:medical_app/data/model/medicine.dart';
 
 class HistoryRepository {
 
-  Future<Null> addUsage(
-      String userId,
+  Future<Null> addUsage(String userId,
       String medicineId,
-      int volume,
-      ) async {
-    final response = await http.post('${Config.url}/user/$userId/history', body: {
+      int volume,) async {
+    final response = await http.post(
+        '${Config.url}/user/$userId/history', body: {
       'medicine_id': medicineId,
       'volume': volume.toString(),
     });
-
+    print(response.body);
     final jsonResponse = json.decode(response.body);
   }
 
   Future<Null> deleteUsage(String usageId) async {
-    final response = await http.delete('${Config.url}/history/$usageId/');
+    final response = await http.delete('${Config.url}/history/$usageId');
+    print(response.body);
   }
 
   Future<Null> updateUsage(Medicine usage) async {
-    final response = await http.post('${Config.url}/history/${usage.usageId}/', body: {
+    final response = await http.post(
+        '${Config.url}/history/${usage.usageId}', body: {
       'volume': usage.volume.toString(),
     });
+
+    print(response.body);
   }
 
   Future<List<Medicine>> fetchUsages(String userId) async {
